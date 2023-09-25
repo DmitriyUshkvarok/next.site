79bad01a23955f3476678c3d9d0a6ac3be562ed1
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-09-25 19:38:18 +0300

    fix blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,28 +1,11 @@
 import ButtonBack from '@/src/components/ButtonBack/ButtonBack';
 import PageTransition from '@/src/components/PageTransition/PageTransition';
-import Link from 'next/link';
-import Image from 'next/image';
 
-
-const Blog =() => {
-
+const Blog = () => {
   return (
     <>
       <PageTransition>
-        <div>
-          {data.map((item) => (
-            // <Link href={`blog/${item._id}`} >
-            <>
-              <div key={item._id}>
-                <div>
-                  <h1>{item.title}</h1>
-                  <p>{item.body}</p>
-                </div>
-              </div>
-            </>
-            // </Link>
-          ))}
-        </div>
+        <div>Blog</div>
         <ButtonBack />
       </PageTransition>
     </>
