75b3911d3872b9b50fde49b6dcde267a5b3e4468
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-10-24 21:15:22 +0300

    delete bag code

--- src/redux/authApi/authSelector.js
+++
@@ -1,7 +0,0 @@
-const authToken = (state) => state.auth?.token;
-
-const authSelector = {
-  authToken,
-};
-
-export default authSelector;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import persisteAuthReducer from './authApi/authSlice';
 import persistedLanguageReducer from './language/languageSlice';
-import { authApi } from './authApi/authApi';
 import { portfolioApi } from './createPortfolioApi/createPortfolioApi';
 import portfolioSlice from './portfolioSlice/portfolioSlice';
 import persisteThemeReducer from './themeSlice/themeSlice';
@@ -17,11 +16,9 @@ import {
 
 const store = configureStore({
   reducer: {
-    auth: persisteAuthReducer,
     language: persistedLanguageReducer,
     portfolio: portfolioSlice,
     theme: persisteThemeReducer,
-    [authApi.reducerPath]: authApi.reducer,
     [portfolioApi.reducerPath]: portfolioApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
@@ -29,7 +26,7 @@ const store = configureStore({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }).concat(authApi.middleware, portfolioApi.middleware),
+    }).concat(portfolioApi.middleware),
 });
 
 export const persistor = persistStore(store);
