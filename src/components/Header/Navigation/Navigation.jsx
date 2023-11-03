a6ea0a48814323a168fff847c5518026d4b60f59
Dmitriy <82167806+DmitriyUshkvarok@users.noreply.github.com>
2023-11-03 14:21:03 +0200

    Update Navigation.jsx

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -49,12 +49,12 @@ const Navigation = () => {
   return (
     <div className={(styles.container, fredericka.className)}>
       <nav className={styles.navigation}>
-        <Link href="/" className={styles.logo}>
+        <div className={styles.logo}>
           <p className={fredericka.className}>Dmitriy Ushkvarok</p>
           <p className={(styles.subTitleLogo, permanentMarker.className)}>
             WebDeveloper
           </p>
-        </Link>
+        </div>
         {ismediaQuery && (
           <div className={styles.authNavWrapper}>
             <AuthNav />
